a5ae309de00af7d88e3641690b8161abdfd41e6e
ageorgh <aghornung@gmail.com>
2024-01-20 17:06:15 +1100

    Initial Commit

---
+++ HeartRate/HeartRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateApp.swift
+//  HeartRate
+//
+//  Created by Alexander Hornung on 20/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
